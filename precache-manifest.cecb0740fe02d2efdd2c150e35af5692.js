e300563325f63480b1662131f2d6ca425560036f
mayankraghuwanshi <mayank1272@hotmail.com>
2019-03-13 22:01:50 +0530

    Updates

---
+++ precache-manifest.cecb0740fe02d2efdd2c150e35af5692.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f778f62c973d5e1f693b",
+    "url": "/static/js/main.3eacba62.chunk.js"
+  },
+  {
+    "revision": "c6800ba3f219d7204b52",
+    "url": "/static/js/2.a657bb28.chunk.js"
+  },
+  {
+    "revision": "f778f62c973d5e1f693b",
+    "url": "/static/css/main.c269df88.chunk.css"
+  },
+  {
+    "revision": "f68e23d3ad4227c6518fbf324e5a1cba",
+    "url": "/index.html"
+  }
+];
